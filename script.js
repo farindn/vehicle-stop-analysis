4afae9af62aa0cd07f3c08485e87a4026b884999
Farin Dian Nugraha <64816837+farindn@users.noreply.github.com>
2025-07-12 01:04:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('view-resume').addEventListener('click', function() {
+    alert('Resume section coming soon!');
+});
+
+document.getElementById('view-projects').addEventListener('click', function() {
+    alert('Projects section coming soon!');
+});
