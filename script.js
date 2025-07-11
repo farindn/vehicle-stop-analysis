0133eff7c37353099036f30d55d7d3050d6529cd
Farin Dian Nugraha <64816837+farindn@users.noreply.github.com>
2025-07-12 01:06:29 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('view-resume').addEventListener('click', function() {
-    alert('Resume section coming soon!');
-});
-
-document.getElementById('view-projects').addEventListener('click', function() {
-    alert('Projects section coming soon!');
-});
